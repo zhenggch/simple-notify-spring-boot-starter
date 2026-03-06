cde1a0c71c95548885a7ec465f7cceb59cede87b
zhenggch <udblfd@163.com>
2026-03-06 13:49:23 +0800

    refactor: extract abstract base class for common logic

---
+++ src/main/java/com/zhenggch/notify/impl/AbstractNotifyService.java
@@ -0,0 +1,32 @@
+package com.zhenggch.notify.impl;
+
+import com.zhenggch.notify.NotifyService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 通知服务抽象基类
+ * 统一处理异常捕获和日志打印
+ */
+@Slf4j
+public abstract class AbstractNotifyService implements NotifyService {
+
+    @Override
+    public void send(String message) {
+        String channelName = this.getClass().getSimpleName();
+        try {
+            // 调用子类的具体发送逻辑
+            doSend(message);
+            log.info("Successfully sent message via {}", channelName);
+        } catch (Exception e) {
+            log.error("Failed to send message via {}", channelName, e);
+        }
+    }
+
+    /**
+     * 执行具体的发送逻辑，由子类实现
+     *
+     * @param message 消息内容
+     * @throws Exception 发送过程中可能抛出的异常
+     */
+    protected abstract void doSend(String message) throws Exception;
+}
--- src/main/java/com/zhenggch/notify/impl/DingTalkNotifyService.java
+++ src/main/java/com/zhenggch/notify/impl/DingTalkNotifyService.java
@@ -2,39 +2,26 @@ package com.zhenggch.notify.impl;
 
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONObject;
-import com.zhenggch.notify.NotifyService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 钉钉通知实现类
  */
-@Slf4j
 @RequiredArgsConstructor
-public class DingTalkNotifyService implements NotifyService {
+public class DingTalkNotifyService extends AbstractNotifyService {
 
-    // 钉钉机器人的 Webhook 地址
     private final String webhook;
 
     @Override
-    public void send(String message) {
-        try {
-            // 1. 构建钉钉机器人需要的 JSON 参数
-            // 参考文档：https://open.dingtalk.com/document/orgapp/custom-robot-access
-            JSONObject json = new JSONObject();
-            json.set("msgtype", "text");
+    protected void doSend(String message) throws Exception {
+        JSONObject json = new JSONObject();
+        json.set("msgtype", "text");
 
-            JSONObject text = new JSONObject();
-            text.set("content", "Notify: " + message);
-            json.set("text", text);
+        JSONObject text = new JSONObject();
+        text.set("content", "Notify: " + message);
+        json.set("text", text);
 
-            // 2. 发送 HTTP POST 请求
-            String result = HttpUtil.post(webhook, json.toString());
-
-            // 3. 打印结果日志
-            log.info("Send DingTalk message result: {}", result);
-        } catch (Exception e) {
-            log.error("Failed to send DingTalk message", e);
-        }
+        // 发送请求，异常和日志交由父类统一处理
+        HttpUtil.post(webhook, json.toString());
     }
 }
